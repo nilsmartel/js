09515e4e3b42def61c38b3f57679e7138ddfd793
Nils Martel <akantorabc@yahoo.de>
2019-07-31 20:52:52 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rust JavaScript Engine
+
+Hardly anything to say just yet.
+Doing it for the fun
--- src/parse/def.rs
+++ src/parse/def.rs
@@ -32,4 +32,19 @@ pub struct FunctionBody {
 /// Either an Expression, if/else pair, for/while loop or return statement
 enum Instruction {
     Return(Option<Box<Expr>>),
+    If {
+        condition: Box<Expr>,
+        body: FunctionBody,
+        else_branch: Option<FunctionBody>,
+    },
+    While {
+        condition: Box<Expr>,
+        body: FunctionBody,
+    },
 }
+
+/// Represents different kinds of for loops
+/// e.g.
+/// for (let i=0; i<len; i++) { ... }
+/// for (let elem of array) { ... }
+enum ForLoop {}
