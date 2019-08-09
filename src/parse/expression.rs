87a3ddac6b504e910be55b57b0a22ea8c2875563
sombrastudios <nils.martel@graphmasters.net>
2019-08-09 16:24:23 +0200

    Start implementation of Expression parsing

--- src/parse/expression.rs
+++ src/parse/expression.rs
@@ -1,9 +1,18 @@
 pub struct Value(f64);
+impl Value {
+    pub fn parse(input: &str) -> IResult<&str, Value> {
+        crate::parse::char_ws('1')(input).map(|(i, _)| (i, Value(1.0)))
+    }
+
+    fn as_expr(self) -> Expr {
+        Expr::Value(self)
+    }
+}
 
 pub enum Expr {
     Mutate(MutationKind, Box<Expr>),
-    And(Box<Expr>, Box<Expr>),
     Or(Box<Expr>, Box<Expr>),
+    And(Box<Expr>, Box<Expr>),
     Xor(Box<Expr>, Box<Expr>),
     Add(Box<Expr>, Box<Expr>),
     Sub(Box<Expr>, Box<Expr>),
@@ -16,12 +25,12 @@ pub enum Expr {
 }
 
 enum MutationKind {
-    Assign, // =
-    AddAssign, // +=
+    Assign,         // =
+    AddAssign,      // +=
     SubtractAssign, // -=
-    ModAssign, // %=
-    MulAssign, // *=
-    DivAssign // /=
+    ModAssign,      // %=
+    MulAssign,      // *=
+    DivAssign,      // /=
 }
 
 use crate::parse::tag_ws;
@@ -33,6 +42,24 @@ impl Expr {
 }
 
 mod parse {
-    use super::Expr;
-    fn
+    use super::*;
+    use crate::parse::{char_ws, tag_ws};
+    fn preceding_not(i: &str) -> IResult<&str, Expr> {
+        if let Ok((i, _)) = char_ws('-')(i) {
+            let (i, e) = exponent(i)?;
+            return Ok((i, Expr::Not(Box::new(e))));
+        }
+
+        value(i)
+    }
+
+    fn exponent(input: &str) -> IResult<&str, Expr> {
+        unimplemented!();
+        // TODO use parse::util::concat here!
+    }
+
+    fn value(i: &str) -> IResult<&str, Expr> {
+        // TODO Alterantive Case of (nestings) here!!
+        Value::parse(i).map(|(i, v)| (i, v.as_expr()))
+    }
 }
