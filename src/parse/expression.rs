f67fa5629d897eecb2f50da83e47744529fadff4
sombrastudios <nils.martel@graphmasters.net>
2019-08-09 16:03:25 +0200

    Include Mutations as Expressions

--- src/parse/expression.rs
+++ src/parse/expression.rs
@@ -1,6 +1,7 @@
 pub struct Value(f64);
 
 pub enum Expr {
+    Mutate(MutationKind, Box<Expr>),
     And(Box<Expr>, Box<Expr>),
     Or(Box<Expr>, Box<Expr>),
     Xor(Box<Expr>, Box<Expr>),
@@ -14,10 +15,24 @@ pub enum Expr {
     // TODO bitshift
 }
 
+enum MutationKind {
+    Assign, // =
+    AddAssign, // +=
+    SubtractAssign, // -=
+    ModAssign, // %=
+    MulAssign, // *=
+    DivAssign // /=
+}
+
 use crate::parse::tag_ws;
 use nom::IResult;
 impl Expr {
     pub fn parse(i: &str) -> IResult<&str, Expr> {
         tag_ws("<expr>")(i).map(|(rest, _)| (rest, Expr::Value(Value(0.0))))
     }
+}
+
+mod parse {
+    use super::Expr;
+    fn
 }
