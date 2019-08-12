5aa232498aa03bc2fa665f390f264de60adea188
sombrastudios <nils.martel@graphmasters.net>
2019-08-12 20:05:04 +0200

    Implement Function Header Parsing

--- src/parse/scope.rs
+++ src/parse/scope.rs
@@ -1,4 +1,4 @@
-use crate::parse::*;
+use crate::{parse::instruction::FunctionBody, parse::*};
 use nom::bytes::complete::tag;
 use nom::IResult;
 
@@ -7,14 +7,6 @@ pub struct Variable {
     identifier: String,
     assign: Option<Box<crate::parse::expression::Expr>>,
 }
-
-#[derive(Debug)]
-pub struct Function {
-    identifier: String,
-    arguments: Vec<String>,
-    body: crate::parse::instruction::FunctionBody,
-}
-// TODO implement Parse for Functions
 
 impl Variable {
     pub fn parse(i: &str) -> IResult<&str, Variable> {
@@ -54,5 +46,53 @@ mod variable_test {
     fn assign() {
         let input = "let xyz = 1 + 1 ";
         assert!(Variable::parse(input).is_ok());
+    }
+}
+
+#[derive(Debug)]
+pub struct Function {
+    identifier: String,
+    arguments: Vec<String>,
+    body: FunctionBody,
+}
+
+impl Function {
+    pub fn parse(input: &str) -> IResult<&str, Function> {
+        use nom::sequence::{delimited, pair, preceded};
+
+        let (input, (identifier, args)) = pair(
+            preceded(tag_ws("function"), ignore_ws(ident)),
+            delimited(
+                char_ws('('),
+                concat(char_ws(','), ignore_ws(ident)),
+                char_ws(')'),
+            ),
+        )(input)?;
+
+        let (input, body) = FunctionBody::parse(input)?;
+
+        Ok((
+            input,
+            Function {
+                identifier: identifier.into(),
+                arguments: args,
+                body,
+            },
+        ))
+    }
+}
+
+#[cfg(test)]
+mod function_test {
+    use super::Function;
+    #[test]
+    fn function_test() {
+        let input = "
+            function one(x, y, z) {
+                return 1
+            }
+            ";
+
+        assert!(Function::parse(input).is_ok());
     }
 }
