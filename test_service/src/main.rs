a83afe0fbf6c4e2fd96b64ac357383fb7470e2ad
sombrastudios <nils.martel@graphmasters.net>
2019-08-12 11:22:31 +0200

    Set up rocket

--- test_service/src/main.rs
+++ test_service/src/main.rs
@@ -1,3 +1,19 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    // TODO set up index page
+    "Hello, world!"
+}
+
+#[post("/parse", data = "<code>")]
+fn parse(code. String) -> String {
+    unimplemented!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
