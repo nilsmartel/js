e804dacbd49bd9a6c120b7c846e6623aa043a09e
sombrastudios <nils.martel@graphmasters.net>
2019-08-13 11:39:12 +0200

    Refine Object spec

--- src/parse/obj.rs
+++ src/parse/obj.rs
@@ -1,9 +1,14 @@
-// Objects
-//
-// Standart JavaScript Objects
+use crate::parse::{expression::Expr, scope::Function};
+use std::collections::HashMap;
 
+/// Represents Parsed JavaScript Object.
+/// Note, that this is _not_ it's final representation,
+/// just an Building Block
 pub enum Object {
     Boolean(bool),
     Float(f64),
     String(String),
+    Array(Vec<Expr>),
+    Map(HashMap<String, Expr>),
+    Closure(Function),
 }
