14d14606a4aec5de8f89e74ae917d4e670d8ffbf
Nils Martel <nils.martel@graphmasters.net>
2020-01-06 18:28:37 +0100

    Implement basic Add Function for Object

--- src/vm/obj/mod.rs
+++ src/vm/obj/mod.rs
@@ -1,45 +1,31 @@
 mod arena;
+use arena::Arena;
 
 use std::rc::Rc;
 
+#[derive(Clone, Debug)]
 enum Value {
+    Undefined,
+    Null,
     Boolean(bool),
     String(Rc<String>),
     Number(f64),
     Reference(usize),
-    // FunctionPointer(usize),
+    FunctionPointer(usize),
+}
+
+impl Value {
+    pub fn add(self, other: Value, _arena: &mut Arena<Object>) -> Value {
+        use Value::*;
+
+        match (self, other) {
+            (Number(a), Number(b)) => Number(a + b),
+            _ => unimplemented!(),
+        }
+    }
 }
 
 enum Object {
     Array(Vec<Object>),
     Class(std::collections::HashMap<String, Object>),
-}
-
-struct Arena {
-    objects: Vec<Object>,
-    free_blocks: Vec<usize>,
-}
-
-impl Arena {
-    fn push(&mut self, o: Object) -> usize {
-        if let Some(index) = self.free_blocks.pop() {
-            self.objects[index] = o;
-            index
-        } else {
-            let index = self.objects.len();
-            self.objects.push(o);
-            index
-        }
-    }
-
-    unsafe fn free(&mut self, index: usize) {
-        assert!(index < self.objects.len());
-
-        if self.objects.len() == index + 1 {
-            self.objects.pop();
-            return;
-        }
-
-        self.free_blocks.push(index);
-    }
 }
