86b7592bc0d33183b156e0e9529aa1da800cfe1b
Nils Martel <nilsmartel@yahoo.de>
2026-07-28 16:11:53 +0200

    Include more tests in suit

---
+++ tests/add.js
@@ -0,0 +1,8 @@
+let result = 1
+
+result += 1
+
+if (result == 2)
+  console.log("Success: result = " + result)
+else
+  console.log("Failure: result = " + result)
---
+++ tests/simple.js
@@ -0,0 +1,6 @@
+let result = 1
+
+if (result == 1)
+  console.log("Success: result = " + result)
+else
+  console.log("Failure: result = " + result)
